2842477d1dad8736dc6c94c6c8b6dea42cc31c0e
densuke <alicedensuke@gmail.com>
2026-03-10 21:44:31 +0900

    wip: テストコード追加

--- src/test/java/com/example/fukushi/service/StockServiceTest.java
+++ src/test/java/com/example/fukushi/service/StockServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.fukushi.service;
 
+import com.example.fukushi.exception.StockNotFoundException;
 import com.example.fukushi.repository.LocationRepository;
 import com.example.fukushi.repository.ProductRepository;
 import com.example.fukushi.repository.StatusRepository;
@@ -13,6 +14,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -67,5 +71,11 @@ public class StockServiceTest {
         stockService.findByFilter(1L, 1L, PageRequest.of(0, 10));
 
         verify(stockRepository).findByProduct_Category_IdAndStatus_Id(eq(1L), eq(1L), any(Pageable.class));
+    }
+
+    @Test
+    void findById_存在しないIDの時_StockNotFoundExceptionがスローされる() {
+        when(stockRepository.findById(99L)).thenReturn(Optional.empty());
+        assertThrows(StockNotFoundException.class, () -> stockService.findById(99L));
     }
  }
